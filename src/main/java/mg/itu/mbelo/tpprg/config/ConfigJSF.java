5007e435b1173e42907b86a63a21e1c16df73380
aramamba <aramamba@PBIO-2827.bbdo.org>
2023-04-01 17:39:35 +0400

    Exercice 1

---
+++ src/main/java/mg/itu/mbelo/tpprg/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.mbelo.tpprg.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author aramamba
+ */
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
